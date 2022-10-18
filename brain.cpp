f573c5c3fafe169e83856b11008eb29b921d2600
3takita <98113361+3takita@users.noreply.github.com>
2022-10-17 20:56:56 -0700

    Update brain.cpp

--- brain.cpp
+++ brain.cpp
@@ -2,31 +2,38 @@
 #include <string>
 #include <stack>
 #include "brain.h"
+//#include "calc.h"
 
 //PreCondition: Number stack is of integer type
 //PostCondition: Computes addition, subtraction, multiplication, division and exponent.
-void brain(const string str, stack<int>& nums) {
+void brain(const string& str, stack<int>& nums) {
     Calc calculator; 
-    if(calculator.is_balanced(str)) {
-        for(int i=0; i<str.length(); i++) {
-            if(isdigit(str[i])) {
-                char c = str[i];
-                int num = calculator.CharToInt(c); 
-                nums.push(num);        
-            }else if(str[i]=='+') {
-                calculator.add(nums, str);
-            }else if(str[i]=='-') {
-                calculator.subtract(nums, str);
-            }else if(str[i]=='*') {
-                calculator.multiply(nums, str);
-            }else if(str[i]=='/') {
-                calculator.divide(nums, str);
-            }else if(str[i]=='^') {
-                calculator.exponent(nums, str);
-            }
-        }
+    bool done = false;
+
+    if(!calculator.is_balanced(str)) {
+        cout<<"\nParanthesis don`t match. Hit enter to continue: ";
+        cin.clear();
+        cin.ignore(123, '\n');
+        string in = calculator.input();
+        brain(in, nums);
     }else{
-        cout<<"\nParanthesis don`t match\n"<<endl;
+        for(int i=0; i<str.length() && calculator.is_balanced(str); i++) {
+        if(isdigit(str[i])) {
+            char c = str[i];
+            int num = calculator.CharToInt(c); //convert char num to int num
+            nums.push(num);         //push converted num unto nums stack
+        }else if(str[i]=='+') {
+            calculator.add(nums, str);
+        }else if(str[i]=='-') {
+            calculator.subtract(nums, str);
+        }else if(str[i]=='*') {
+            calculator.multiply(nums, str);
+        }else if(str[i]=='/') {
+            calculator.divide(nums, str);
+        }else if(str[i]=='^') {
+            calculator.exponent(nums, str);
+        }
     }
-    cout<<"It evaluates to: "<<nums.top()<<endl;
+    }
+    cout<<"\nIt evaluates to: "<<nums.top()<<endl<<endl;
 }
